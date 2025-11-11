966b814cc39b67077e0d5e3e3597ee2efa5ee6e1
GitLab CI <ci@dana.id>
2025-11-11 06:57:16 +0000

    Updated DANA Self Integration Test Scenarios

--- test/python/payment_gateway/cancel_order_test.py
+++ test/python/payment_gateway/cancel_order_test.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import time
+import asyncio
 from uuid import uuid4
 from datetime import datetime, timedelta, timezone
 from dana.utils.snap_configuration import SnapConfiguration, AuthSettings, Env
@@ -13,10 +14,13 @@ from dana.exceptions import *
 from helper.api_helpers import get_headers_with_signature, execute_and_assert_api_error
 from helper.util import get_request, with_delay, retry_on_inconsistent_request
 from helper.assertion import *
+from payment_gateway.payment_pg_util import automate_payment_pg
 
 title_case = "CancelOrder"
 create_order_title_case = "CreateOrder"
 json_path_file = "resource/request/components/PaymentGateway.json"
+user_phone_number = "0811742234"
+user_pin = "123321"
 merchant_id = os.environ.get("MERCHANT_ID", "1234567890")  # Default to a test merchant ID if not set
 
 configuration = SnapConfiguration(
@@ -37,8 +41,9 @@ with ApiClient(configuration) as api_client:
 def generate_partner_reference_no():
     return str(uuid4())
 
-@retry_on_inconsistent_request(max_retries=3,delay_seconds=2)
-def create_test_order(partner_reference_no):
+@retry_on_inconsistent_request(max_retries=3, delay_seconds=2)
+def create_test_order_init():
+    data_order = []
     """Helper function to create a test order"""
     case_name = "CreateOrderApi"
     
@@ -46,42 +51,61 @@ def create_test_order(partner_reference_no):
     json_dict = get_request(json_path_file, create_order_title_case, case_name)
     
     # Set the partner reference number
-    json_dict["partnerReferenceNo"] = partner_reference_no
+    json_dict["partnerReferenceNo"] = generate_partner_reference_no()
     json_dict["validUpTo"] = (datetime.now().astimezone(timezone(timedelta(hours=7))) + timedelta(seconds=300)).strftime('%Y-%m-%dT%H:%M:%S+07:00')
 
     # Convert the request data to a CreateOrderRequest object
     create_order_request_obj = CreateOrderByApiRequest.from_dict(json_dict)
     
     # Make the API call
-    api_instance.create_order(create_order_request_obj)
-
-@retry_on_inconsistent_request(max_retries=3,delay_seconds=2)
-def create_test_order_canceled(partner_reference_no):
-    """Helper function to create a test order with short expiration date to have canceled status"""
-    case_name = "CreateOrderApi"
-    
-    # Get the request data from the JSON file
-    json_dict = get_request(json_path_file, create_order_title_case, case_name)
-    
-    # Set the partner reference number
-    json_dict["partnerReferenceNo"] = partner_reference_no
-
-    # Set the expiration time to 1 second from now
-    json_dict["validUpTo"] = (datetime.now().astimezone(timezone(timedelta(hours=7))) + timedelta(seconds=300)).strftime('%Y-%m-%dT%H:%M:%S+07:00')
-    
-    # Convert the request data to a CreateOrderRequest object
-    create_order_request_obj = CreateOrderByApiRequest.from_dict(json_dict)
-    
-    # Make the API call
-    api_instance.create_order(create_order_request_obj)
+    api_response = api_instance.create_order(create_order_request_obj)
+    data_order.append(api_response.partner_reference_no)
+    data_order.append(api_response.web_redirect_url)
+    print(f"Created order with reference number: {api_response.partner_reference_no}")
+    print(f"Web redirect URL: {api_response.web_redirect_url}")
+    return data_order
 
 @pytest.fixture(scope="module")
 def test_order_reference_number():
     """Fixture that creates a test order once per module and shares the reference number"""
-    partner_reference_no = generate_partner_reference_no()
+    data_order = create_test_order_init()
+    print(f"\nCreating shared test order with reference number: {data_order[0]}")
+    return data_order[0]
+
+@pytest.fixture(scope="module")
+def test_order_reference_number_refund():
+    """Fixture that creates a test order once per module and shares the reference number"""
+    partner_reference_no = create_test_order_refunded()
     print(f"\nCreating shared test order with reference number: {partner_reference_no}")
-    create_test_order(partner_reference_no)
-    return partner_reference_no 
+    return partner_reference_no
+
+def create_test_order_paid():
+    """Helper function to create a test order with short expiration date to have paid status"""
+    data_order = create_test_order_init()
+    asyncio.run(automate_payment_pg(
+        phone_number=user_phone_number,
+        pin=user_pin,
+        redirectUrlPayment=data_order[1],
+        show_log=True
+    ))
+    return data_order[0]
+
+def create_test_order_refunded():
+    """Helper function to create a test order with refunded status"""
+    partner_reference_no = create_test_order_paid()
+    # Refund the order
+    case_name = "RefundOrderValidScenario"
+    json_dict = get_request(json_path_file, "RefundOrder", case_name)
+    # Set the partner reference number
+    json_dict["originalPartnerReferenceNo"] = partner_reference_no
+    json_dict["partnerRefundNo"] = partner_reference_no
+    
+    # Create the request object from the JSON dictionary
+    refund_order_request = RefundOrderRequest.from_dict(json_dict)
+
+    # Call the refund API endpoint with the request object
+    api_instance.refund_order(refund_order_request)
+    return partner_reference_no
 
 @with_delay()
 @retry_on_inconsistent_request(max_retries=3, delay_seconds=1)
@@ -270,14 +294,16 @@ def test_cancel_order_with_agreement_not_allowed():
         pytest.fail("Expected ForbiddenException but the API call give another exception")
 
 @with_delay()
-@pytest.mark.skip(reason="skipped by request: scenario CancelOrderInvalidTransactionStatus")
-def test_cancel_order_with_invalid_transaction_status():
+def test_cancel_order_with_invalid_transaction_status(test_order_reference_number_refund):
     """Should fail to cancel the order when transaction status is invalid"""
     # Cancel order
     case_name = "CancelOrderInvalidTransactionStatus"
     
     # Get the request data from the JSON file
     json_dict = get_request(json_path_file, title_case, case_name)
+    
+    # Set the correct partner reference number
+    json_dict["originalPartnerReferenceNo"] = test_order_reference_number_refund
     
     # Convert the request data to a CancelOrderRequest object
     cancel_order_request_obj = CancelOrderRequest.from_dict(json_dict)
@@ -287,8 +313,8 @@ def test_cancel_order_with_invalid_transaction_status():
         api_instance.cancel_order(cancel_order_request_obj)
 
         pytest.fail("Expected ForbiddenException but the API call succeeded")
-    except ForbiddenException as e:
-        assert_fail_response(json_path_file, title_case, case_name, e.body, {"partnerReferenceNo": "4035715"})
+    except NotFoundException as e:
+        assert_fail_response(json_path_file, title_case, case_name, e.body, {"partnerReferenceNo": "4045700"})
     except:
         pytest.fail("Expected ForbiddenException but the API call give another exception")
 
